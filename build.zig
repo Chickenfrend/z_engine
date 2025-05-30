be5d9825a73f72f8feb7d6c38a070bab7c003c4f
Peter Lovett <pelovett@gmail.com>
2025-05-29 21:00:50 -0700

    Adding opengl test

--- build.zig
+++ build.zig
@@ -1,70 +1,131 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-    const maybe_override_registry = b.option([]const u8, "override-registry", "Override the path to the Vulkan registry used for the examples");
 
-    const registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml");
-
-    const triangle_exe = b.addExecutable(.{
-        .name = "triangle",
-        .root_source_file = b.path("src/triangle.zig"),
+    // This creates a "module", which represents a collection of source files alongside
+    // some compilation options, such as optimization mode and linked system libraries.
+    // Every executable or library we compile will be based on one or more modules.
+    const lib_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
-        .link_libc = true,
         .optimize = optimize,
     });
-    b.installArtifact(triangle_exe);
+
+    // We will also create a module for our other entry point, 'main.zig'.
+    const exe_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Modules can depend on one another using the `std.Build.Module.addImport` function.
+    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
+    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
+    exe_mod.addImport("z_engine_lib", lib_mod);
+
+    // Now, we will create a static library based on the module we created above.
+    // This creates a `std.Build.Step.Compile`, which is the build step responsible
+    // for actually invoking the compiler.
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "z_engine",
+        .root_module = lib_mod,
+    });
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
+    // This creates another `std.Build.Step.Compile`, but this one builds an executable
+    // rather than a static library.
+    const exe = b.addExecutable(.{
+        .name = "z_engine",
+        .root_module = exe_mod,
+    });
 
     if (target.result.os.tag == .macos) {
         // Add include paths
-        triangle_exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/glfw/include" });
-        triangle_exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-loader/include" });
-        triangle_exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-headers/include" });
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/glfw/include" });
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-loader/include" });
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-headers/include" });
 
         // Add library paths
-        triangle_exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/glfw/lib" });
-        triangle_exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-loader/lib" });
+        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/glfw/lib" });
+        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/opt/vulkan-loader/lib" });
     }
 
-    triangle_exe.linkSystemLibrary("glfw");
+    exe.linkSystemLibrary("glfw");
+    exe.linkFramework("OpenGL");
+    exe.linkLibC();
 
-    const registry_path: std.Build.LazyPath = if (maybe_override_registry) |override_registry|
-        .{ .cwd_relative = override_registry }
-    else
-        registry;
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
 
-    const vulkan = b.dependency("vulkan_zig", .{
-        .registry = registry_path,
-    }).module("vulkan-zig");
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
 
-    triangle_exe.root_module.addImport("vulkan", vulkan);
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
 
-    const vert_cmd = b.addSystemCommand(&.{
-        "glslc",
-        "--target-env=vulkan1.2",
-        "-o",
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
     });
-    const vert_spv = vert_cmd.addOutputFileArg("vert.spv");
-    vert_cmd.addFileArg(b.path("shaders/triangle.vert"));
-    triangle_exe.root_module.addAnonymousImport("vertex_shader", .{
-        .root_source_file = vert_spv,
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_module = exe_mod,
     });
 
-    const frag_cmd = b.addSystemCommand(&.{
-        "glslc",
-        "--target-env=vulkan1.2",
-        "-o",
-    });
-    const frag_spv = frag_cmd.addOutputFileArg("frag.spv");
-    frag_cmd.addFileArg(b.path("shaders/triangle.frag"));
-    triangle_exe.root_module.addAnonymousImport("fragment_shader", .{
-        .root_source_file = frag_spv,
-    });
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    const triangle_run_cmd = b.addRunArtifact(triangle_exe);
-    triangle_run_cmd.step.dependOn(b.getInstallStep());
-
-    const triangle_run_step = b.step("run-triangle", "Run the triangle example");
-    triangle_run_step.dependOn(&triangle_run_cmd.step);
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
---
+++ src/main.zig
@@ -0,0 +1,23 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+const c = @cImport({
+    @cDefine("GLFW_INCLUDE_NONE", {});
+    @cInclude("GLFW/glfw3.h");
+    @cInclude("OpenGL/gl.h");
+});
+
+pub fn main() !void {
+    _ = c.glfwInit();
+    const window = c.glfwCreateWindow(800, 600, "Test", null, null);
+    _ = c.glfwMakeContextCurrent(window);
+
+    while (c.glfwWindowShouldClose(window) == 0) {
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+        c.glfwSwapBuffers(window);
+        c.glfwPollEvents();
+    }
+
+    c.glfwTerminate();
+    return;
+}
