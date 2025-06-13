711d1ef6b37b651fd39951b503cd5bc29259fda3
Andrea Del Rio <chickenfrend@gmail.com>
2025-06-12 20:14:00 -0700

    Added new triangle stuff

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,131 @@
 //! By convention, main.zig is where your main function lives in the case that
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
-const builtin = @import("builtin");
 
+const std = @import("std");
+const Shader = @import("./rendering/ShaderLib.zig");
 const c = @cImport({
-    if (builtin.os.tag == .macos) {
-        @cInclude("OpenGL/gl.h");
-        @cDefine("GLFW_INCLUDE_NONE", {});
-        @cInclude("GLFW/glfw3.h");
-    } else {
-        @cInclude("GL/gl.h");
-        @cDefine("GLFW_INCLUDE_NONE", {});
-        @cInclude("GLFW/glfw3.h");
-    }
+    @cDefine("GLFW_INCLUDE_NONE", {});
+    @cInclude("GLFW/glfw3.h");
+    @cInclude("OpenGL/gl3.h");
 });
 
+const WindowSize = struct {
+    pub const width: u32 = 800;
+    pub const height: u32 = 600;
+};
+
+// This main functions does a lot. It creates shaders, links them, opens a window, and draws a triangle.
+// Probably we could split these aparts and have modules dedicated to shaders, a module for shapes, and so on.
 
 pub fn main() !void {
     _ = c.glfwInit();
-    const window = c.glfwCreateWindow(800, 600, "Test", null, null);
+    // Without these hints the shaders wouldn't compile. They might need to be changed depending on your system and openGL version.
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    _ = c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+
+    // I think this is for mac only.
+    _ = c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+
+    const window = c.glfwCreateWindow(WindowSize.width, WindowSize.height, "Test Window", null, null);
     _ = c.glfwMakeContextCurrent(window);
 
+    _ = c.glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var arena_allocator_state = std.heap.ArenaAllocator.init(allocator);
+    defer arena_allocator_state.deinit();
+    const arena_allocator = arena_allocator_state.allocator();
+    // This is the creation of the shader program.
+    const shaderProgram: Shader = Shader.create(arena_allocator, "src/rendering/shaders/triangle_shader.vs", "src/rendering/shaders/triangle_shader.fs");
+
+    // These are the twelve vertices that make up the square.
+    const vertices = [12]f32{
+        0.5,
+        0.5,
+        0.0,
+        0.5,
+        -0.5,
+        0.0,
+        -0.5,
+        -0.5,
+        0.0,
+        -0.5,
+        0.5,
+        0.0,
+    };
+    // These are the indices for the two triangles that make up the square.
+    const indices = [6]u32{
+        0, 1, 3,
+        1, 2, 3,
+    };
+
+    // This is the vertex buffer id, the vertex array object id, and the element buffer object ID. Later we assign a vertex buffer to the vertex buffer id.
+    // The vertex buffer lets us send a lot of vertex information to the GPU at once.
+    var VBO: c_uint = undefined;
+    var VAO: c_uint = undefined;
+    var EBO: c_uint = undefined;
+    c.glGenVertexArrays(1, &VAO);
+    defer c.glDeleteVertexArrays(1, &VAO);
+    c.glGenBuffers(1, &VBO);
+    defer c.glDeleteBuffers(1, &VBO);
+    c.glGenBuffers(1, &EBO);
+    defer c.glDeleteBuffers(1, &EBO);
+
+    // We start by binding the vertex array object.
+    c.glBindVertexArray(VAO);
+
+    // Then we bind the vertex buffer object. This is where we send the vertex data to the GPU.
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, VBO);
+    c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, c.GL_STATIC_DRAW);
+
+    // Now we bind the element buffer object. This is where we send the indices to the GPU.
+    c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, EBO);
+    c.glBufferData(c.GL_ELEMENT_ARRAY_BUFFER, @sizeOf(u32) * indices.len, &indices, c.GL_STATIC_DRAW);
+
+    // This tells openGL how to interpret the vertex data. It defines the layout of the vertex data in the buffer.
+    // These parameters are confusing. But, info on them can be found here: https://learnopengl.com/Getting-started/Hello-Triangle
+    // I'll just say that the 3 is because we're using vec3
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), null);
+    c.glEnableVertexAttribArray(0);
+
+    // This is the loop that keeps the window open and draws to the screen.
     while (c.glfwWindowShouldClose(window) == 0) {
+
+        // Input
+        processInput(window);
+
+        // Render
+        c.glClearColor(0.2, 0.3, 0.3, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        // Draw the square
+        shaderProgram.use();
+        c.glBindVertexArray(VAO);
+        c.glDrawElements(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_INT, null);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
     }
 
     c.glfwTerminate();
+
     return;
 }
+
+fn frame_buffer_size_callback(window: ?*c.GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
+    _ = window;
+    c.glViewport(0, 0, width, height);
+}
+
+fn processInput(window: ?*c.GLFWwindow) void {
+    if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS) {
+        c.glfwSetWindowShouldClose(window, 1);
+    }
+}
+
+/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
+const lib = @import("zig-opengl_lib");
---
+++ src/rendering/ShaderLib.zig
@@ -0,0 +1,119 @@
+// Shader library
+
+const std = @import("std");
+
+const c = @cImport({
+    @cDefine("GLFW_INCLUDE_NONE", {});
+    @cInclude("GLFW/glfw3.h");
+    @cInclude("OpenGL/gl3.h");
+});
+
+const Shader = @This();
+
+ID: c_uint,
+
+pub fn create(arena: std.mem.Allocator, vertex_path: []const u8, fragment_path: []const u8) Shader {
+    // Vertex Shader Creation
+    // Vertex shaders run once per vertex and best I can tell, determine where the vertex will be on the screen.
+    // So, it converts from 3D coordinates to 2D coordinates at the very minimum.
+    // At least I think. It might just position them in 3D space?
+    var vertexShader: c_uint = undefined;
+    vertexShader = c.glCreateShader(c.GL_VERTEX_SHADER);
+    defer c.glDeleteShader(vertexShader);
+
+    // This is the setup for the fragment shader. The fragment shader runs once per pixel and determines the color of each pixel.
+    // Simple!
+    var fragmentShader: c_uint = undefined;
+    fragmentShader = c.glCreateShader(c.GL_FRAGMENT_SHADER);
+    defer c.glDeleteShader(fragmentShader);
+
+    // Storing the result of selfExeDirPathAlloc in a variable to avoid calling it multiple times.
+    const arena_path = std.fs.selfExeDirPathAlloc(arena) catch unreachable;
+    const full_vertex_path = std.fs.path.join(arena, &.{
+        arena_path,
+        vertex_path,
+    }) catch unreachable;
+
+    std.debug.print("Full vertex path: {s}\n", .{full_vertex_path});
+
+    const full_fragment_path = std.fs.path.join(arena, &.{
+        arena_path,
+        fragment_path,
+    }) catch unreachable;
+
+    std.debug.print("Full fragment path: {s}\n", .{full_fragment_path});
+
+    const vertex_file = std.fs.openFileAbsolute(full_vertex_path, .{}) catch unreachable;
+    const vertexShaderSource = vertex_file.readToEndAlloc(arena, 10 * 1024) catch unreachable;
+    std.debug.print("Vertex shader source: {s}\n", .{vertexShaderSource});
+
+    const vertexShaderSourceZ = arena.dupeZ(u8, vertexShaderSource);
+
+    const fragment_file = std.fs.openFileAbsolute(full_fragment_path, .{}) catch unreachable;
+    const fragmentShaderSource = fragment_file.readToEndAlloc(arena, 10 * 1024) catch unreachable;
+
+    const fragmentShaderSourceZ = arena.dupeZ(u8, fragmentShaderSource);
+
+    // Build and compile the vertex shader
+    // Vertex shaders run once per vertex and best I can tell, determine where the vertex will be on the screen.
+    // So, it converts from 3D coordinates to 2D coordinates at the very minimum.
+    // At least I think. It might just position them in 3D space?
+    c.glShaderSource(vertexShader, 1, @ptrCast(&vertexShaderSourceZ), null);
+    c.glCompileShader(vertexShader);
+
+    var success: c_int = undefined;
+
+    // In C this would be a simple char array.
+    // In zig it's a bit more complicated. We use a fixed-size array of u8.
+    // The underscore before the u8 after the equal sign tells the compiler to infer the array size.
+    // 512 in the value tells it to multiply the size by 512 bytes. The 0 means that the array is initialized with zeros.
+    var infoLog: [512]u8 = [_]u8{0} ** 512;
+    c.glGetShaderiv(vertexShader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(vertexShader, 512, 0, &infoLog);
+        std.log.err("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{s}\n", .{infoLog[0 .. std.mem.indexOfScalar(u8, &infoLog, 0) orelse infoLog.len]});
+    }
+
+    // Fragment Shader setup
+    c.glShaderSource(fragmentShader, 1, @ptrCast(&fragmentShaderSourceZ), null);
+    c.glCompileShader(fragmentShader);
+    c.glGetShaderiv(fragmentShader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(fragmentShader, 512, 0, &infoLog);
+        std.log.err("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{s}\n", .{infoLog[0 .. std.mem.indexOfScalar(u8, &infoLog, 0) orelse infoLog.len]});
+    }
+
+    // This is the creation of the shader program.
+    const shaderProgram = c.glCreateProgram();
+
+    // These lines are where we attach the two shaders to the shader program, and then link them.
+    c.glAttachShader(shaderProgram, vertexShader);
+    c.glAttachShader(shaderProgram, fragmentShader);
+    c.glLinkProgram(shaderProgram);
+
+    // Checking for linking errors.
+    std.debug.print("About to check for linking errors", .{});
+    c.glGetProgramiv(shaderProgram, c.GL_LINK_STATUS, &success);
+    if (success == 0) {
+        c.glGetProgramInfoLog(shaderProgram, 512, 0, &infoLog);
+        std.log.err("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{s}\n", .{infoLog[0 .. std.mem.indexOfScalar(u8, &infoLog, 0) orelse infoLog.len]});
+    }
+
+    return Shader{ .ID = shaderProgram };
+}
+
+pub fn use(self: Shader) void {
+    c.glUseProgram(self.ID);
+}
+
+pub fn setBool(self: Shader, name: [*c]const u8, value: bool) void {
+    c.glUniform1i(c.glGetUniformLocation(self.ID, name), if (value) 1 else 0);
+}
+
+pub fn setInt(self: Shader, name: [*c]const u8, value: u32) void {
+    c.glUniform1i(c.glGetUniformLocation(self.ID, name), @as(c_int, value));
+}
+
+pub fn setFloat(self: Shader, name: [*c]const u8, value: f32) void {
+    c.glUniform1f(c.glGetUniformLocation(self.ID, name), value);
+}
