6c60aeae196347c40e3ea9c245fe0db43f8c707d
Peter Lovett <pelovett@gmail.com>
2026-01-15 21:51:37 -0800

    Add state and animations

--- src/ecs/state.zig
+++ src/ecs/state.zig
@@ -1,28 +1,32 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-
 const MAX_ENTITIES = 5_000;
 const MAX_COMPONENTS = 50_000;
 
-const RectangleEntity = struct  {
-    pub var position_idx: u64;
+const RectangleEntity = struct {
+    position_idx: u64,
 };
 
 const PositionComponent = struct {
-    pub var x: f64;
-    pub var y: f64;
-}
+    x: f64,
+    y: f64,
+};
 
-const State = struct {
-    pub var rectangles: [*]RectangleEntity;
+pub const GlobalState = struct {
+    clock: std.time.Timer,
+    game_state: GameState,
+};
 
-    pub var positions: [*]PositionComponent;
+pub const GameState = struct {
+    rectangles: []RectangleEntity,
 
-    pub fn init(allo: Allocator) State {
-        return State{
+    positions: []PositionComponent,
+
+    pub fn init(allo: Allocator) !GameState {
+        return .{
             .rectangles = try allo.alloc(RectangleEntity, MAX_ENTITIES),
             .positions = try allo.alloc(PositionComponent, MAX_COMPONENTS),
-        }
+        };
     }
-}
+};
