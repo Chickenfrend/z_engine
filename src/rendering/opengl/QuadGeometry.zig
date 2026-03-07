360df3070f5641e025a66bc515462015bb463872
Benjamin Lain <chickenfrend@gmail.com>
2026-03-07 12:42:08 -0800

    Creating some opengl specific geometry and shit

---
+++ src/rendering/opengl/QuadGeometry.zig
@@ -0,0 +1,109 @@
+const builtin = @import("builtin");
+
+const c = if (builtin.os.tag == .macos)
+    @cImport({
+        @cDefine("GLFW_INCLUDE_NONE", "");
+        @cInclude("GLFW/glfw3.h");
+        @cInclude("OpenGL/gl3.h");
+    })
+else
+    @cImport({
+        @cDefine("GLFW_INCLUDE_NONE", "");
+        @cDefine("GL_GLEXT_PROTOTYPES", "");
+        @cInclude("GLFW/glfw3.h");
+        @cInclude("GL/gl.h");
+        @cInclude("GL/glext.h");
+    });
+
+pub const QuadGeometry = struct {
+    VAO: c_uint,
+    VBO: c_uint,
+    EBO: c_uint,
+    instance_VBO: c_uint,
+
+    // Position (x, y, z) + UV (u, v)
+    const vertices = [20]f32{
+        0.5, 0.5, 0.0, 0.128, 0.21, // right top
+        0.5, -0.5, 0.0, 0.128, 0.1, // right bottom
+        -0.5, -0.5, 0.0, 0.06, 0.1, // left bottom
+        -0.5, 0.5, 0.0, 0.06, 0.21, // left top
+    };
+
+    const indices = [6]u32{
+        0, 1, 3,
+        1, 2, 3,
+    };
+
+    pub fn init() QuadGeometry {
+        var geo: QuadGeometry = undefined;
+
+        c.glGenVertexArrays(1, &geo.VAO);
+        c.glGenBuffers(1, &geo.VBO);
+        c.glGenBuffers(1, &geo.EBO);
+        c.glGenBuffers(1, &geo.instance_VBO);
+
+        c.glBindVertexArray(geo.VAO);
+
+        // Setup vertex data
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, geo.VBO);
+        c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices)), &vertices, c.GL_STATIC_DRAW);
+
+        c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, geo.EBO);
+        c.glBufferData(c.GL_ELEMENT_ARRAY_BUFFER, @sizeOf(@TypeOf(indices)), &indices, c.GL_STATIC_DRAW);
+
+        // Position attribute
+        c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 5 * @sizeOf(f32), null);
+        c.glEnableVertexAttribArray(0);
+
+        // UV attribute (from VBO)
+        c.glVertexAttribPointer(1, 2, c.GL_FLOAT, c.GL_FALSE, 5 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
+        c.glEnableVertexAttribArray(1);
+
+        // Setup for instance data buffer
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, geo.instance_VBO);
+
+        // Model matrix columns (each mat4 takes 4 attribute locations)
+        // We'll send the full 4x4 matrix per instance
+        var i: u32 = 0;
+        while (i < 4) : (i += 1) {
+            const loc = 2 + i;  // locations 2, 3, 4, 5
+            c.glEnableVertexAttribArray(loc);
+            c.glVertexAttribPointer(
+                loc, 
+                4, 
+                c.GL_FLOAT, 
+                c.GL_FALSE, 
+                @sizeOf([16]f32),  // stride = size of mat4
+                @ptrFromInt(i * @sizeOf([4]f32))  // offset to each column
+            );
+            c.glVertexAttribDivisor(loc, 1);  // This makes it instanced
+        }
+
+        c.glBindVertexArray(0);
+
+        return geo;
+    }
+
+    pub fn deinit(self: *QuadGeometry) void {
+        c.glDeleteVertexArrays(1, &self.VAO);
+        c.glDeleteBuffers(1, &self.VBO);
+        c.glDeleteBuffers(1, &self.EBO);
+        c.glDeleteBuffers(1, &self.instance_VBO);
+    }
+
+
+    pub fn drawInstanced(self: *const QuadGeometry, instance_count: c_int) void {
+        c.glBindVertexArray(self.VAO);
+        c.glDrawElementsInstanced(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_INT, null, instance_count);
+    }
+
+    pub fn updateInstanceData(self: *const QuadGeometry, matrices: []const [16]f32) void {
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, self.instance_VBO);
+        c.glBufferData(
+            c.GL_ARRAY_BUFFER,
+            @intCast(matrices.len * @sizeOf([16]f32)),
+            matrices.ptr,
+            c.GL_DYNAMIC_DRAW
+        );
+    }
+} ;
---
+++ src/rendering/opengl/opengl_rendering.zig
@@ -0,0 +1,138 @@
+// File containing main renderer loop
+
+const Shader = @import("ShaderLib.zig");
+const SquareGeometry = @import("./Square.zig").SquareGeometry;
+const QuadGeometry = @import("./QuadGeometry.zig").QuadGeometry;
+const Square = @import("./Square.zig").Square;
+const Texture = @import ("./Texture.zig").Texture;
+
+const std = @import("std");
+const zm = @import("zm");
+
+const builtin = @import("builtin");
+
+const c = if (builtin.os.tag == .macos)
+    @cImport({
+        @cDefine("GLFW_INCLUDE_NONE", "");
+        @cInclude("GLFW/glfw3.h");
+        @cInclude("OpenGL/gl3.h");
+    })
+else
+    @cImport({
+        @cDefine("GLFW_INCLUDE_NONE", "");
+        @cDefine("GL_GLEXT_PROTOTYPES", "");
+        @cInclude("GLFW/glfw3.h");
+        @cInclude("GL/gl.h");
+        @cInclude("GL/glext.h");
+    });
+
+pub const WindowSize = struct {
+    pub const width: u32 = 800;
+    pub const height: u32 = 600;
+};
+
+// Rendering is currently only rendering squares. We should modify this and
+// add batch rendering.
+//
+// An easy thing to do would be to add some kind of abstract geometry struct.
+fn flattenMat4(mat: [4][4]f32) [16]f32 {
+    return @as(*const [16]f32, @ptrCast(&mat)).*;
+}
+
+// This is really the "openGl" specific renderer. This should be moved and replaced with a 
+// backend-agnostic interface.
+pub const RenderPipeline = struct {
+    shader: Shader,
+    texture: Texture,
+    projection: [4][4]f32,
+    allocator: std.mem.Allocator,
+    matrices: std.ArrayList([16]f32),
+    geometry: QuadGeometry,
+
+    // The square geometry should be changed when we implement sprites. Probably.
+    // I think this should take a flag, called "instanced" or something, which would let it toggle
+    // between rendering instanced and uniform/non-instanced.
+    // I'm not sure exactly how that should be worked out or how instanced vs non instanced stuff
+    // should be organized. Right now (02/26/2026) the VBO/VAO is associated with the geometry,
+    // not the render pipeline.
+    pub fn render(self: *RenderPipeline, squares: []const Square) !void {
+        // Render
+        // This clears the screen
+        c.glClearColor(0.2, 0.3, 0.3, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        // Reuse memory
+        self.matrices.clearRetainingCapacity();
+
+        for (squares) |square| {
+            const square_trans = zm.Mat4f.translation(square.position[0], square.position[1], 0.0);
+            const scale = zm.Mat4f.scaling(square.width, square.height, 1.0);
+
+            // You could add rotation and stuff onto this.
+            // This has to be transposed here because it isn't tranposed by the setMat4f
+            // function like the projection matrix is.
+            // I'm not sure how efficient this is.
+            const modelM = square_trans.multiply(scale).transpose();
+
+            // Add to the list of things to be drawn.
+            try self.matrices.append(self.allocator, flattenMat4(modelM.data));
+        }
+
+        // Send the instance data
+        self.geometry.updateInstanceData(self.matrices.items);
+
+        // Draw the square
+        self.shader.use();
+        c.glActiveTexture(c.GL_TEXTURE0);
+        c.glBindTexture(c.GL_TEXTURE_2D, self.texture.id);
+        self.shader.setInt("textureSampler", 0);
+        self.shader.setMat4f("projection", flattenMat4(self.projection));
+
+        var bound_texture: c.GLint = 0;
+        c.glGetIntegerv(c.GL_TEXTURE_BINDING_2D, &bound_texture);
+        var bound_program: c.GLint = 0;
+        c.glGetIntegerv(c.GL_CURRENT_PROGRAM, &bound_program);
+        var blend_enabled: c.GLboolean = 0;
+        c.glGetBooleanv(c.GL_BLEND, &blend_enabled);
+        // Draw em all. We're sending the instance count here.
+        self.geometry.drawInstanced(@intCast(squares.len));
+    }
+
+    pub fn init(allocator: std.mem.Allocator) RenderPipeline {
+        const shaderProgram: Shader = Shader.create(
+            allocator,
+            "src/rendering/shaders/position_shader.vs",
+            "src/rendering/shaders/triangle_shader.frag",
+        );
+
+        const texture = Texture.initFromFile(allocator, "src/font.png");
+        const geometry = QuadGeometry.init();
+
+        const projM = zm.Mat4f.orthographicRH(0, WindowSize.width, WindowSize.height, 0, -1.0, 1.0);
+
+        return RenderPipeline{
+            .shader = shaderProgram,
+            .projection = projM.data,
+            .allocator = allocator,
+            .matrices = .empty,
+            .texture = texture,
+            .geometry = geometry,
+        };
+    }
+
+    pub fn draw(self: *RenderPipeline) void {
+        c.glBindVertexArray(self.VAO);
+        c.glDrawElements(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_INT, null);
+    }
+
+    pub fn cleanup(self: *RenderPipeline) void {
+        self.matrices.deinit(self.allocator);
+        self.texture.deinit();
+        c.glDeleteProgram(self.shader.ID);
+    }
+};
+
+fn calculateGreenValue(time: f64) f32 {
+    const timeCasted: f32 = @floatCast(time);
+    return @sin(timeCasted) / 2.0 + 0.5;
+}
